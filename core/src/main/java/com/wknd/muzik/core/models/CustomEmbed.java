9956b79ac10fe940694b2c9df1539730b5d14878
Shivangi <shivangi2418@gmail.com>
2020-10-15 11:26:12 +0000

    Changes for exporter

--- core/src/main/java/com/wknd/muzik/core/models/CustomEmbed.java
+++ core/src/main/java/com/wknd/muzik/core/models/CustomEmbed.java
@@ -1,10 +1,8 @@
 package com.wknd.muzik.core.models;
 
-import javax.inject.Inject;
-
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.Via;
@@ -16,16 +14,19 @@ import com.adobe.cq.wcm.core.components.models.Embed;
 
 import lombok.experimental.Delegate;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = {Embed.class, CustomEmbed.class}, resourceType = "wkndmuzik/components/embed", defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = SlingHttpServletRequest.class, adapters = { Embed.class,
+		CustomEmbed.class }, resourceType = "wkndmuzik/components/embed", defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = "jackson", extensions = "json")
 public class CustomEmbed implements Embed {
-	
-	@Self @Via(type = ResourceSuperType.class)
+
+	@Self
+	@Via(type = ResourceSuperType.class)
 	@Delegate // Delegate all our methods to the Core Components Embed class
 	private Embed embed;
-	
+
 	@ValueMapValue
 	@Optional
-    protected String thumbnail;
+	protected String thumbnail;
 
 	public String getThumbnail() {
 		return thumbnail;
--- core/src/main/java/com/wknd/muzik/core/models/CustomTeaser.java
+++ core/src/main/java/com/wknd/muzik/core/models/CustomTeaser.java
@@ -3,10 +3,8 @@ package com.wknd.muzik.core.models;
 import com.adobe.cq.wcm.core.components.models.Teaser;
 import lombok.experimental.Delegate;
 
-import javax.inject.Inject;
-
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
@@ -15,36 +13,39 @@ import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = {Teaser.class,CustomTeaser.class}, resourceType = "wkndmuzik/components/teaser",defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class CustomTeaser implements Teaser{
+@Model(adaptables = SlingHttpServletRequest.class, adapters = { Teaser.class,
+		CustomTeaser.class }, resourceType = "wkndmuzik/components/teaser", defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = "jackson", extensions = "json")
+public class CustomTeaser implements Teaser {
 
-	@Self @Via(type = ResourceSuperType.class)
+	@Self
+	@Via(type = ResourceSuperType.class)
 	@Delegate // Delegate all our methods to the Core Components Teaser class
 	@Optional
-    private Teaser teaser;
-	
-	@ValueMapValue
-	@Optional
-    protected String backgroundImg;
-	
-	@ValueMapValue
-	@Optional
-    protected String showsDate;
-	
-	@ValueMapValue
-	@Optional
-    protected String blurBackground;
+	private Teaser teaser;
 
-    public String getBackgroundImg() {
-        return backgroundImg;
-    }
-    
-    public String getShowsDate() {
-        return showsDate;
-    } 
-    
-    public String getBlurBackground() {
-        return blurBackground;
-    } 
-    
+	@ValueMapValue
+	@Optional
+	protected String backgroundImg;
+
+	@ValueMapValue
+	@Optional
+	protected String showsDate;
+
+	@ValueMapValue
+	@Optional
+	protected String blurBackground;
+
+	public String getBackgroundImg() {
+		return backgroundImg;
+	}
+
+	public String getShowsDate() {
+		return showsDate;
+	}
+
+	public String getBlurBackground() {
+		return blurBackground;
+	}
+
 }
